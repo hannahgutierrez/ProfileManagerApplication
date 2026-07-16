9df4effa05efe389f538da7af8572f3a34710b64
Hannah Gutierrez <hannah_gutierrez@dlsu.edu.ph>
2026-07-16 22:58:56 +0800

    Added imports in FriendRepository

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/repository/FriendRepository.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/repository/FriendRepository.java
@@ -1,4 +1,11 @@
 package ph.edu.dlsu.lbycpob.profilemanagerapplication.repository;
 
-public interface FriendRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+import ph.edu.dlsu.lbycpob.profilemanagerapplication.model.Friend;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface FriendRepository extends JpaRepository<Friend, UUID> {
 }
