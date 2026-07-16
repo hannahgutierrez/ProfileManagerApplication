1fa5b60ebb78f51ff6eea33622bc0147d5b9db16
Hannah Gutierrez <hannah_gutierrez@dlsu.edu.ph>
2026-07-16 22:59:58 +0800

    Added imports in ProfileRepository

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/repository/ProfileRepository.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/repository/ProfileRepository.java
@@ -1,4 +1,12 @@
 package ph.edu.dlsu.lbycpob.profilemanagerapplication.repository;
 
-public interface ProfileRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import ph.edu.dlsu.lbycpob.profilemanagerapplication.model.Profile;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ProfileRepository extends JpaRepository<Profile, UUID>
+{
 }
