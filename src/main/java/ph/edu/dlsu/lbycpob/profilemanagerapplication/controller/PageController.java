e2d67abbae457697803b1f10293f3a10c66896f8
Hannah Gutierrez <hannah_gutierrez@dlsu.edu.ph>
2026-07-16 23:15:42 +0800

    Added import in PageController

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/controller/PageController.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/controller/PageController.java
@@ -1,4 +1,7 @@
 package ph.edu.dlsu.lbycpob.profilemanagerapplication.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class PageController {
 }
