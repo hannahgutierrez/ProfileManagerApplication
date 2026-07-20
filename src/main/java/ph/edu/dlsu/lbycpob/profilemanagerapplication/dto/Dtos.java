8ea11257758967fabe258fd9ddcdfa00d09abc7f
alexiabungay <alexia_bungay@dlsu.edu.ph>
2026-07-20 12:48:24 +0800

    [Dtos] added imports

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/dto/Dtos.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/dto/Dtos.java
@@ -1,4 +1,9 @@
 package ph.edu.dlsu.lbycpob.profilemanagerapplication.dto;
 
+import ph.edu.dlsu.lbycpob.profilemanager.model.Profile;
+
+import java.util.List;
+import java.util.UUID;
+
 public class Dtos {
 }
