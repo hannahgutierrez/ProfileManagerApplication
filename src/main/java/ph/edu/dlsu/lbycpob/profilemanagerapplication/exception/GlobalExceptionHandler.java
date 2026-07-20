6566a70733c46283e427b5ceb9140c9ba32a3835
alexiabungay <alexia_bungay@dlsu.edu.ph>
2026-07-20 12:52:02 +0800

    [GlobalExceptionHandler] added imports

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/exception/GlobalExceptionHandler.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/exception/GlobalExceptionHandler.java
@@ -1,4 +1,13 @@
 package ph.edu.dlsu.lbycpob.profilemanagerapplication.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import ph.edu.dlsu.lbycpob.profilemanager.dto.Dtos.ApiError;
+
+import java.util.NoSuchElementException;
+
 public class GlobalExceptionHandler {
 }
